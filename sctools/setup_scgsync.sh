0d96dfde581acb15b3a5b927679f2c811f6a6173
Jane Lusby <jlusby42@gmail.com>
2018-07-06 15:25:07 -0700

    Use gitroot in convertToPwd

--- sctools/setup_scgsync.sh
+++ sctools/setup_scgsync.sh
@@ -6,6 +6,8 @@ set -o xtrace
 # shellcheck disable=SC1090
 . ~/.scalerc
 
-git remote add buildvm "ssh://$GIT_USER@$BUILDVM_HOSTNAME:$SRC_ROOT"
 run_buildvm git config receive.denyCurrentBranch ignore
 scp post-receive "$BUILDVM_HOSTNAME:$SRC_ROOT/.git/hooks/"
+
+cd "$GIT_ROOT" || exit 1
+git remote add buildvm "ssh://$GIT_USER@$BUILDVM_HOSTNAME:$SRC_ROOT"
