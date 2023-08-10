bd3a1ac9515e20c9bd5f340a088c8bad11766cef
Jane Losare-Lusby <jlusby@yaah.dev>
2023-08-10 15:51:41 -0700

    remove ancient unused config files

--- Scripts/pull-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-runQuery "select build_id,testsuite_result_id from test_failure_detail where comment LIKE 'JL%' and reviewed = 0 order by end_time desc" \
-    | awk '{print "http://releaseengineering.lab.local/builds/automated/" $1 "/logs/test/" $2"/"}' \
-    | xargs -n1 -i pulllogs.sh {}
--- sshkeys.sh
+++ sshkeys.sh
@@ -1,13 +1,2 @@
-echo "Remember not to be lazy this time, put the password you use to encrypt the key into your password manager"
-
-edkey="$HOME/.ssh/id_ed25519"
-rsakey="$HOME/.ssh/id_rsa"
-
-if [ ! -e $edkey ]; then
-    ssh-keygen -t ed25519 -a 100 -f $edkey
-fi
-
-if [ ! -e $rsakey ]; then
-    ssh-keygen -t rsa -b 4096 -o -a 100 -f $rsakey
-fi
+echo "Generate your ssh keys directly in 1password"
 
