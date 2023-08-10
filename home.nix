ed9cffdf7dd6d582d03c74028b739d675a68806f
Jane Losare-Lusby <jlusby@yaah.dev>
2023-08-10 16:03:37 -0700

    add initial home.nix config for user specific setup

---
+++ home.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "yaahc";
+  home.homeDirectory = "/home/yaahc";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  home.packages = [
+  	neovim
+	ripgrep
+	git
+	discord
+  ];
+}
